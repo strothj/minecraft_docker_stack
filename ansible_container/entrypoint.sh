ae543088e40846e1ab0d1923f65c2e2e10a8ea0e
strothj <git@jasons.io>
2024-10-25 02:34:05 -0400

    feat: provision SSH key on Vultr

--- ansible_container/entrypoint.sh
+++ ansible_container/entrypoint.sh
@@ -4,7 +4,7 @@ source /opt/venv/bin/activate
 
 # Handle the provisioning of the gateway.
 if [ "$1" = 'provision' ]; then
-  readonly cloud_provider=$(xargs < "$CLOUD_PROVIDER_FILE")
+  readonly cloud_provider=$(xargs < /run/secrets/cloud_provider)
   exec ansible-playbook -i localhost "playbook.provision.$cloud_provider.yml"
 fi
 
