c02741263c88dbb9336f7b65e62003133dce3adb
strothj <git@jasons.io>
2024-10-25 20:08:37 -0400

    fix: resolve Ansible warning about use of implicit localhost

--- ansible_container/entrypoint.sh
+++ ansible_container/entrypoint.sh
@@ -5,7 +5,7 @@ source /opt/venv/bin/activate
 # Handle the provisioning of the gateway.
 if [ "$1" = 'provision' ]; then
   readonly cloud_provider=$(xargs < /run/secrets/cloud_provider)
-  exec ansible-playbook -i localhost "playbook.provision.$cloud_provider.yml"
+  exec ansible-playbook -i inventory.localhost.yml "playbook.provision.$cloud_provider.yml"
 fi
 
 exec "$@"
