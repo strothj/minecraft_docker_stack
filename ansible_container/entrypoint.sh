3c84a0a8ad5a0c1ef79deb30709457d611b60ce3
strothj <git@jasons.io>
2024-10-29 23:07:26 -0400

    feat: support additional arguments to Ansible scripts

--- ansible_container/entrypoint.sh
+++ ansible_container/entrypoint.sh
@@ -5,19 +5,19 @@ source /opt/venv/bin/activate
 # Handle the provisioning of the gateway.
 if [ "$1" = 'provision' ]; then
   readonly cloud_provider=$(xargs < /run/secrets/cloud_provider)
-  exec ansible-playbook -i inventory.localhost.yml "playbook.provision.$cloud_provider.yml"
+  exec ansible-playbook -i inventory.localhost.yml "playbook.provision.$cloud_provider.yml" "${@:2}"
 fi
 
 # Handle setup of the gateway.
 if [ "$1" = 'setup' ]; then
   readonly cloud_provider=$(xargs < /run/secrets/cloud_provider)
-  exec ansible-playbook -i inventory.$cloud_provider.yml "playbook.setup.yml"
+  exec ansible-playbook -i inventory.$cloud_provider.yml "playbook.setup.yml" "${@:2}"
 fi
 
 # Handle the teardown of the gateway.
 if [ "$1" = 'teardown' ]; then
   readonly cloud_provider=$(xargs < /run/secrets/cloud_provider)
-  exec ansible-playbook -i inventory.localhost.yml "playbook.teardown.$cloud_provider.yml"
+  exec ansible-playbook -i inventory.localhost.yml "playbook.teardown.$cloud_provider.yml" "${@:2}"
 fi
 
 exec "$@"
