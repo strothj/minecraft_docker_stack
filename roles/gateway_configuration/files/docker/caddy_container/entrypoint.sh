ec02f200db4ca5d6784870c9c1a6c50a94679930
strothj <jason@jasons.io>
2024-11-10 22:14:14 -0500

    feat: set mode on gateway Caddy data folders

--- roles/gateway_configuration/files/docker/caddy_container/entrypoint.sh
+++ roles/gateway_configuration/files/docker/caddy_container/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 chown -R caddy:caddy /config
 chown -R caddy:caddy /data
+chmod 700 /config /data
 
 gosu caddy /usr/bin/caddy environ
 exec gosu caddy /usr/bin/caddy run --config /config.json
